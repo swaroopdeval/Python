2bd877d99caac500856e5ca12096f5ad60966255
Swaroop Singh Deval <ssinghdeval@gmail.com>
2015-01-25 02:57:04 +0530

    Create 2.py

---
+++ HackerRank/Python-tutorials/Basic-calculator/2.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = float(input())
+b = float(input())
+
+print("%.2f" % (a+b))
+print("%.2f" % (a-b))
+print("%.2f" % (a*b))
+print("%.2f" % (a/b))
+print("%.2f" % (a//b))
