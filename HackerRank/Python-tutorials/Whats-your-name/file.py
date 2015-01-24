a6dc194a1b9252419bc0cb107ca1c4a69a7fd5b1
Swaroop Singh Deval <ssinghdeval@gmail.com>
2015-01-25 02:20:31 +0530

    Create file.py

---
+++ HackerRank/Python-tutorials/Whats-your-name/file.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+firstname = raw_input()
+lastname = raw_input()
+
+print ("Hello {0} {1}! You just delved into python.".format(firstname,lastname))
